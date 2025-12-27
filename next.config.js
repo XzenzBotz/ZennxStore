dc93b5457ad46615932459b65d9c6fb3799877be
Akmall Nz <hapeadvan1404@gmail.com>
2025-12-27 20:32:22 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,19 @@
 const path = require("path")
 
-const imagesDomains =
-  process.env.UPLOAD_DOMAINS
+const imagesDomains = process.env.UPLOAD_DOMAINS
     ? process.env.UPLOAD_DOMAINS.split(",")
     : []
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
-  images: {
-    domains: imagesDomains,
-  },
+    swcMinify: true,
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, "style")],
+    },
+    images: {
+        domains: imagesDomains,
+    },
 }
 
 module.exports = nextConfig
